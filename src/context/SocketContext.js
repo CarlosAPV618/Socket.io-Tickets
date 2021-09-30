bcb0c744c4cee758edaad0c4c34b616ed3cf5c59
Carlos Paredes <carlos618314@gmail.com>
2021-09-30 14:32:16 -0500

    Conexion a la API desplegada

--- src/context/SocketContext.js
+++ src/context/SocketContext.js
@@ -4,7 +4,7 @@ import useSocket from "../hooks/useSocket";
 export const SocketContext = createContext()
 
 export const SocketProvider = ({children}) => {
-    const [socket, online] = useSocket('http://localhost:4000')
+    const [socket, online] = useSocket('https://cola-tickets-socket-io.herokuapp.com/')
 
     return ( 
         <SocketContext.Provider
